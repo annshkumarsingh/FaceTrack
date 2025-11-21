e3256de54aa224de76db626205c38e1e04d44d08
annshkumarsingh <anshkrajpoot@gmail.com>
2025-11-22 00:27:48 +0530

    Updated CORS for netifly and render deploys.

--- backend/app.py
+++ backend/app.py
@@ -25,9 +25,10 @@ backend_url = os.getenv("BACKEND_URL")
 app = FastAPI()
 
 origins = [
+    "https://facetrack-ai.netlify.app/",
+    "https://facetrack-lia6.onrender.com",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://localhost:3000",
     "http://localhost:8000",  
     "http://127.0.0.1:8000",
 ]
