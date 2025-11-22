9ff65266154c1a22b44ec5a4f68bcbf92709ad62
annshkumarsingh <anshkrajpoot@gmail.com>
2025-11-22 20:11:33 +0530

    Changed database config.

--- backend/database/database.py
+++ backend/database/database.py
@@ -8,9 +8,16 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("POSTGRE_DATABASE_URL")
 
+if "sslmode" not in DATABASE_URL:
+    DATABASE_URL += "?sslmode=require"
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=5,         # prevent too many connections
+    max_overflow=0,      # do NOT create extra connections
+    pool_pre_ping=True,  # auto-reconnect
+)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
